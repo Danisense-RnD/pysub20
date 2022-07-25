95ce20ad57f06c89f7165ecbfe63b64515462844
paul_cool <paul@coolautomation.com>
2022-07-25 11:05:44 +0300

    hatch instead of pbr

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 if __name__ == "__main__":
     setup(
-        setup_requires=['pbr'],
-        pbr=True,
+
     )
--- src/sub20/__init__.py
+++ src/sub20/__init__.py
@@ -4,4 +4,4 @@ from sub20._errors import (
     I2CDevicesNotFoundError
 )
 
-from sub20.device import SUBDevice
+__version__ = '0.0.1dev21'
