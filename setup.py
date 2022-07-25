fdae03663b08ad770909cdb1e1e223e60fa668ec
paul_cool <paul@coolautomation.com>
2022-07-25 09:26:58 +0300

    clean up

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
-
+long_description = open('README.rst').read()
 if __name__ == "__main__":
     setup(
         setup_requires=['pbr'],
         pbr=True,
+        long_description=long_description
     )
